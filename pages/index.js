c26046d9cf3afca9470047cfc85cb38f1a65fe6f
btm_yomogita <h-yomogita@b-tm.co.jp>
2023-05-17 20:51:45 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Index!TEST!COMMIT!!</title>
+        <title>Index!TEST!COMMIT!!how?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main page="index"></Main>
