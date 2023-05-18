248f09cf21fa225e60f33816c971976a633b3c8f
ymgt840 <yomo1022@gmail.com>
2023-05-18 21:18:01 +0900

    learning links

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import classes from './Header.module.css'
+
+export function Header() {
+  return (
+        <header className={classes.header}>
+            <Link href="/">
+                <p className={classes.anchor}>Index</p>
+            </Link>
+            <Link href="/about">
+                <p className={classes.anchor}>about</p>
+            </Link>
+        </header>
+  )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
+import { Header } from '../components/Header'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Header></Header>
       <Head>
         <title>Index!TEST!COMMIT!!how?now,who user?</title>
         <link rel="icon" href="/favicon.ico" />
