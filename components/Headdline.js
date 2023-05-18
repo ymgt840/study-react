76f2f2455c2c79e010b6d4f3f877ef5a2747b614
ymgt840 <yomo1022@gmail.com>
2023-05-18 20:59:31 +0900

    learning css.module

--- components/Headdline.js
+++ components/Headdline.js
@@ -1,13 +1,13 @@
-import styles from '../styles/Home.module.css'
+import classes from './HeadLine.module.css'
 
 export function Headlines(props) {
   return (
         <div>
-            <h1 className={styles.title}>
+            <h1 className={classes.title}>
                 {props.page} page
             </h1>
 
-            <p className={styles.description}>
+            <p className={classes.description}>
             Get started by editing {props.children}
             </p>
             <button onClick={props.onClick}>test btn</button>
--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,12 @@
-import styles from '../styles/Home.module.css'
+import classes from './Main.module.css'
 import { TopLinks } from '../components/TopLinks'
 import { Headlines } from '../components/Headdline'
 
 export function Main(props) {
   return (
-      <main className={styles.main}>
+      <main className={classes.main}>
         <Headlines page={props.page}>
-          <code className={styles.code}>pages/{props.page}.js</code>
+          <code className={classes.code}>pages/{props.page}.js</code>
         </Headlines>
         <TopLinks />
       </main>
