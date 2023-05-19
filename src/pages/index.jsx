54bbce39d833baf0791de9d5f27f041a44c7607f
ymgt840 <yomo1022@gmail.com>
2023-05-19 19:45:33 +0900

    learning useEffect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 export default function Home() {
   const foo = 'foo';
@@ -13,6 +13,13 @@ export default function Home() {
     alert(foo)
     e.preventDefault()
   }, [])
+
+  useEffect(() => { // mount際に効く
+    document.body.style.backgroundColor = "lightblue"
+    return () => { // unmount
+      document.body.style.backgroundColor = ""
+    }
+  }, [])  
 
   return (
     <div className={styles.container}>
