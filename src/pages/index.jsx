fda6cf633735a35edece509dcb6204a9233d8ff1
ymgt840 <yomo1022@gmail.com>
2023-05-19 20:00:03 +0900

    learning useState

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,13 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
-  const foo = 'foo';
+  const [cnt, setCnt] = useState(1)
 
   const handleClick = useCallback((e) => { // useCallbackで再レンダリングを防ぐ
-    console.log(e)
-    alert(foo)
-    e.preventDefault()
+    setCnt(cnt => cnt + 1)
   }, [])
 
   useEffect(() => { // mount際に効く
@@ -24,12 +22,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header></Header>
-        <a 
-          href='/about'
+        <h1>{cnt}</h1>
+        <button
           onClick={handleClick}
         >
         ボタン
-        </a>
+        </button>
       <Head>
         <title>Index!TEST!COMMIT!!how?now,who user?</title>
       </Head>
