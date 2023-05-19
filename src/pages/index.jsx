e92544a791af9dee24741b6027be881417b1fcb4
ymgt840 <yomo1022@gmail.com>
2023-05-19 18:00:16 +0900

    learn onClick

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,11 +3,26 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
+import { useCallback } from 'react'
 
 export default function Home() {
+  const foo = 'foo';
+
+  const handleClick = useCallback((e) => { // useCallbackで再レンダリングを防ぐ
+    console.log(e)
+    alert(foo)
+    e.preventDefault()
+  }, [])
+
   return (
     <div className={styles.container}>
       <Header></Header>
+        <a 
+          href='/about'
+          onClick={handleClick}
+        >
+        ボタン
+        </a>
       <Head>
         <title>Index!TEST!COMMIT!!how?now,who user?</title>
       </Head>
