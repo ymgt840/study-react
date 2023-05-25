07cc32b42a45ee3bf18921c3ea896f4306095bc7
ymgt840 <yomo1022@gmail.com>
2023-05-25 19:32:53 +0900

    learning useMemo and useRouter

---
+++ src/hooks/useBgColor.jsx
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router'
+import { useEffect, useMemo } from 'react'
+
+export const useBgColor = () => {
+  const router  = useRouter() // routeのプロパティを取得する
+  const bgColor = useMemo(() => {
+    return router.pathname === "/" ? "lightblue" : "pink"
+  }, [router.pathname])// 第2引数でpathName変更をキャッチする
+
+  useEffect(() => { // mount際に効く
+    document.body.style.backgroundColor = bgColor
+    return () => { // unmount
+      document.body.style.backgroundColor = ""
+    }
+  }, [bgColor])
+}
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,13 +2,13 @@ import Head from 'next/head'
 import 'src/styles/globals.css'
 import { useCounter } from 'src/hooks/useCounter'
 import { useHandleArray } from 'src/hooks/useHandleArray'
-import { useBgLightBlue } from 'src/hooks/useBgLightBlue'
+import { useBgColor } from 'src/hooks/useBgColor'
 
 function MyApp({ Component, pageProps }) {
   // stateのリフトアップ (pages全体で状態を共有する)
   const counter = useCounter()
   const handleArray = useHandleArray()
-  useBgLightBlue()
+  useBgColor()
   return (
     <>
       <Head>
